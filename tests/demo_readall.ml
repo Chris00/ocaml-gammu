5626b0a4f223091fb205a83dc7136a1d42baf43f
Pierre Hauweele <antegallya@gmail.com>
2010-09-12 03:04:40 +0200

    In demo_readall : move "read all sms" code in a "read_all" function.

--- tests/demo_readall.ml
+++ tests/demo_readall.ml
@@ -5,6 +5,24 @@ open Printf
 
 module G = Gammu
 
+let read_all s folder =
+  let begin_time = Unix.gettimeofday () in
+  let c =
+    try
+      G.SMS.fold s ~folder
+        ~on_err:(fun loc e ->
+          printf "Failed to get SMS next to location %i: %s\n"
+            loc (G.string_of_error e);
+          flush stdout;
+          Unix.sleep 1)
+        (fun c m -> print_multi_sms m; c + 1) 0;
+    with G.Error G.NOTSUPPORTED | G.Error G.NOTIMPLEMENTED ->
+      failwith "Sorry but your phone doesn't support GetNext \
+                or it is not implemented.\n"
+  in
+  let elapsed_time = Unix.gettimeofday () -. begin_time in
+  printf "\n%i message(s) read in %fsecs\n" c elapsed_time
+
 let () =
   try
     parse_args ();
@@ -13,22 +31,7 @@ let () =
     prepare_phone s;
     print_string "Start reading from folder: ";
     let folder = int_of_string (read_line ()) in
-    let begin_time = Unix.gettimeofday () in
-    let c =
-      try
-        G.SMS.fold s ~folder
-          ~on_err:(fun loc e ->
-            printf "Failed to get SMS next to location %i: %s\n"
-              loc (G.string_of_error e);
-            flush stdout;
-            Unix.sleep 1)
-          (fun c m -> print_multi_sms m; c + 1) 0;
-      with G.Error G.NOTSUPPORTED | G.Error G.NOTIMPLEMENTED ->
-        failwith "Sorry but your phone doesn't support GetNext \
-                 or it is not implemented.\n"
-    in
-    let elapsed_time = Unix.gettimeofday () -. begin_time in
-    printf "\n%i message(s) read in %fsecs\n" c elapsed_time
+    read_all s folder
   with G.Error e -> printf "Error: %s\n" (G.string_of_error e)
 (* TODO: add trap to disconnect *)
 
