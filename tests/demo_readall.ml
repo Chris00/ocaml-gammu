f8d2ef370e73678e14e3365184ef6e69aad2d5c3
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-09-12 11:40:03 +0200

    Slightly improving the code of the readall SMS demo.

--- tests/demo_readall.ml
+++ tests/demo_readall.ml
@@ -12,19 +12,17 @@ let print_multi_sms multi_sms =
   printf "Message_number: %d\n" sms.SMS.location;
   printf "Is in inbox: %B\n" sms.SMS.inbox_folder;
   printf "Number: %s\n" sms.SMS.number;
-  printf "Date and time: %s\n"
-    (DateTime.os_date_time sms.SMS.date_time);
+  printf "Date and time: %s\n" (DateTime.os_date_time sms.SMS.date_time);
   printf "Status : %s\n" (string_of_sms_status sms.SMS.state);
-  if (sms.SMS.udh_header.SMS.udh = SMS.No_udh) then
+  if sms.SMS.udh_header.SMS.udh = SMS.No_udh then
     (* There's no udh so text is raw in the sms message. *)
-    print_string sms.SMS.text
+    printf "%s" sms.SMS.text
   else begin
     (* There's an udh so we have to decode the sms. *)
     let multi_info = SMS.decode_multipart multi_sms in
     Array.iter (fun info -> print_string info.SMS.buffer) multi_info.SMS.entries
   end;
-  print_newline ();
-  flush stdout
+  printf "\n%!"
 
 let () =
   parse_args ();
@@ -38,16 +36,17 @@ let () =
     try
       SMS.fold s ~folder
         ~on_err:(fun loc e ->
-          printf "Failed to get SMS next to location %i: %s\n"
+          printf "Failed to get SMS next to location %i: %s\n%!"
             loc (string_of_error e);
-          flush stdout;
           Unix.sleep 1)
         (fun c m -> print_multi_sms m; c + 1) 0;
-    with Error NOTSUPPORTED | Error NOTIMPLEMENTED ->
-      failwith "Sorry but your phone doesn't support GetNext \
-                 or it is not implemented.\n"
+    with
+    | Error NOTSUPPORTED ->
+      failwith "Sorry but your phone doesn't support GetNext"
+    | Error NOTIMPLEMENTED ->
+      failwith "Sorry but GetNext is not implemented."
   in
   let elapsed_time = Unix.gettimeofday () -. begin_time in
-  Printf.printf "\n%i message(s) read in %fsecs\n" c elapsed_time
+  printf "\n%i message(s) read in %fsecs\n" c elapsed_time
 (* TODO: add trap to disconnect *)
 
